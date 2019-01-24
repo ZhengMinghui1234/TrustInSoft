b82604ebcf11400d7be720289e9ac541e29d9d4b
zhengminghui <982079709@qq.com>
2019-01-24 14:33:50 +0800

    Update uninitialized.c

--- examples/01-Uninitialized-access/uninitialized.c
+++ examples/01-Uninitialized-access/uninitialized.c
@@ -2,7 +2,7 @@ int main(void)
 {
   int array[5] = {13, 7, 42, 0, 1};
   int i, n;
-
+  int a = 10;
   for (i = 0; i < 5; i++) {
     /* The variable 'n' is declared at line 4, but is never
        initialized before usage. The analyzer emit then an alarm
@@ -12,6 +12,10 @@ int main(void)
        that has not been initialized. */
     n += array[i];
   }
-
+  if(a > 0) {
+    a += a;
+  } else {
+    a *= a;
+  }
   return 0;
 }
