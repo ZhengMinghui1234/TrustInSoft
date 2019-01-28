85827c91f2a4a583fa2db1534690afcf550f4b23
zhengminghui <982079709@qq.com>
2019-01-28 18:19:35 +0800

    Update uninitialized.c

--- examples/01-Uninitialized-access/uninitialized.c
+++ examples/01-Uninitialized-access/uninitialized.c
@@ -1,10 +1,4 @@
-struct S {
-  int x;
-};
-
-void f(struct S s);
-
-void main() {
-  struct S s;
-  f(s); // warn: passed-by-value arg contain uninitialized data
+void test(int z) {
+  if (z == 0)
+    int x = 1 / z; // warn
 }
